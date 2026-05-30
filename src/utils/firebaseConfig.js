a0b16fd560577c620f166424c4511aa16cd6d245
camiloripetrov-svg <prietovillarrealcamiloalejandr@gmail.com>
2026-05-29 21:40:03 -0500

    use env variables for firebase config

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,19 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { initializeAuth, browserPopupRedirectResolver } from "firebase/auth"; // CAMBIO: Usaremos initializeAuth nativo
+import { initializeAuth, browserPopupRedirectResolver } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZdcW4g9apX8NtfG5EHSsRtwC4ogg0BEs",
-  authDomain: "nexus-1f56f.firebaseapp.com",
-  projectId: "nexus-1f56f",
-  storageBucket: "nexus-1f56f.firebasestorage.app",
-  messagingSenderId: "925466332795",
-  appId: "1:925466332795:web:71cce1c5bb8f48d7459cde",
-  measurementId: "G-SR2WVCTLGS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-// SOLUCIÓN: Forzamos una inicialización limpia aislando los solucionadores de redirección del navegador
 export const auth = initializeAuth(app, {
   popupRedirectResolver: browserPopupRedirectResolver
 });
